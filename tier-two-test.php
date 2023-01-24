0d93f3da1da3dc170009c764e51b7dc1c9af5d5e
aliashiquemohammad <aliashiquemohammad@gmail.com>
2023-01-24 16:20:22 +0545

    Update tier-two-test.php

--- tier-two-test.php
+++ tier-two-test.php
@@ -19,3 +19,33 @@ function remove_exclusion_recaptcha_js_from_wp_rocket_cache( $files ) {
 }
 add_filter( 'rocket_lazyload_exclude', 'remove_exclusion_recaptcha_js_from_wp_rocket_cache', 999 );
 
+/** 
+* Question Three 1
+*
+* Which URLs are purged when that function runs?
+* 
+* Answer: 
+*
+* The URLs affected with rocket_clean_post() function include:
+* - Post Permalink
+* - Pagination
+* - Associated Taxonomy Terms
+* - Associated Author 
+
+/** 
+* Question Three 2
+*
+* A customer reported an issue that requires you to debug that function. How would you log the URLs that are purged when rocket_clean_post() runs?
+* 
+* Answer: 
+*
+* There are various methods for grabbing purge URLs, but I have used one of the hooks that are in the WP Rocket plugin.
+*/
+function debug_error_log_for_post_purge_urls( $post, $purge_urls ){
+	
+	error_log( 'Purged URLs: ' . implode( ', ', $purge_urls ) );
+}
+add_action( 'after_rocket_clean_post', 'debug_error_log_for_post_purge_urls', 999, 2 );
+
+// Furthermore, to log these purge URLs, I have used an error_log() function so that we can directly see it in our error log file. 
+
